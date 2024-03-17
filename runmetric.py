ca61a2a75f04499e7e851527c4f032dce5f96d85
Sarvesh Bhatnagar <sarveshbhatnagar@Sarveshs-MacBook-Pro.local>
2024-03-17 02:50:53 -0700

    Updating runmetric to be able to read standard run.py file

--- runmetric.py
+++ runmetric.py
@@ -5,4 +5,16 @@ metric_repos = [
 ]
 
 
-# TODO - Test the metric run for the given repositories.
+### Code to execute run.py in mbox_word_count/run.py
+
+import os
+import subprocess
+
+# TODO Clear metrics.txt
+
+for repo in metric_repos:
+    print(f"Running {repo}...")
+    subprocess.run(["python3", "{}/run.py".format(repo)])
+    print(f"{repo} done.")
+
+print("All repositories done.")
